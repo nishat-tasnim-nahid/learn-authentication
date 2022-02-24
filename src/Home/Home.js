944df1149d50e180c5e1a7f6e8a723478bb980ee
Tasnim Nahid <nnnahidt@gmail.com>
2022-02-24 18:34:20 +0600

    add home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <h1>this is home.</h1>
+            <h2>Hello world</h2>
         </div>
     );
 };
